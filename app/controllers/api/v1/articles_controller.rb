6f154bb501f89ef6423cf282b6bf6a1149a0c058
Alysia Wu <alysiawu>
2020-08-03 20:44:25 -0700

    rest

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,9 +1,45 @@
 module Api
-    module v1
-        class ArticlesControler < ApplicationController
+    module V1
+        class ArticlesController < ApplicationController
             def index
-                articles = Articles.order('created_at DESC');
-                render json: { status: 'SUCCESS', message: 'Loaded articles', data: articles}, status:ok
+                articles = Article.order('created_at DESC');
+                render json: { status: 'SUCCESS', message: 'Loaded articles', data: articles},status: :ok
+            end
+
+            def show
+                article = Article.find(params[:id])
+                render json: { status: 'SUCCESS', message: 'Loaded article', data: article},status: :ok
+            end
+
+            def create
+                article = Article.new(article_params)
+                if article.save
+                    render json: { status: 'SUCCESS', message: 'Saved Article', data: article},status: :ok
+                else 
+                    render json: { status: 'ERROR', message: 'Article not saved', data: article.errors},status: :unprocessable_entity
+                end
+                    
+            end
+
+            def destroy
+                article = Article.find(params[:id])
+                article.destroy
+                render json: { status: 'SUCCESS', message: 'Deleted Article', data: article},status: :ok
+            end
+
+            def update 
+                article = Article.find(params[:id])
+                if article.update_attributes(article_params) # update_attributes 
+                    render json: { status: 'SUCCESS', message: 'Updated Article', data: article},status: :ok
+                else
+                    render json: { status: 'ERROR', message: 'Article not updated', data: article.errors},status: :unprocessable_entity
+                end
+            end 
+
+            private 
+
+            def article_params
+                params.permit(:title, :body)
             end
         end
     end
